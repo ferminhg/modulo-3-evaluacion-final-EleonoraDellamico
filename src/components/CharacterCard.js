3269abc9e083f24ad3128330a8f22c54e1c4d49d
Eleonora Dell'Amico <eleonoradellamico4@gmail.com>
2020-11-23 20:07:08 +0100

    Add card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,8 @@
-/* const CharacterCard = () => {
-
-
+const CharacterCard = (props) => {
+  <article>
+    <img src={props.url} alt="Main character" />
+    <h3>{props.name}</h3>
+    <h4>{props.species}</h4>
+  </article>
 }
-export default CharacterCard;  */
+export default CharacterCard;  
